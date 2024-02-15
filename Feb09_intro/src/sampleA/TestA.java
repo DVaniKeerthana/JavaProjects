5ec7a63c297d94aa02195d367edae582a9f09d70
Vani Keerthana <dandavanikeerthana@gmail.com>
2024-02-15 13:21:01 +0530

    Initial commit

---
+++ Feb09_intro/src/sampleA/TestA.java
@@ -0,0 +1,14 @@
+package sampleA;
+
+public class TestA {
+
+	public static void main(String[] args) {
+				System.out.print("Hello");
+				System.out.print(" Everyone");
+				System.out.println("..!!!");
+				System.out.println("How");
+				System.out.println("are");
+				System.out.print("you all??");
+					}
+
+				}
