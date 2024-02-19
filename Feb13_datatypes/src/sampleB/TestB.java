79eb2a1291d67138f7d0f7a1c96664d537839752
Vani Keerthana <dandavanikeerthana@gmail.com>
2024-02-19 19:00:16 +0530

    Git ignore
    Add metadata files in git Ignore

---
+++ Feb13_datatypes/src/sampleB/TestB.java
@@ -0,0 +1,32 @@
+package sampleB;
+
+public class TestB {
+
+	public static void main(String[] args) {
+		
+			byte a= 77;
+			System.out.println(a);
+			
+			short b= 3744;
+			System.out.println(b);
+			
+			boolean c= true;
+			System.out.println(c);
+			
+			int d= -648427331;
+			System.out.println(d);
+			
+			long e=7427412879333l;
+			System.out.println(e);
+
+			
+			float f=2.2244f;
+			System.out.println(f);
+			
+			
+			double g= 4.3222627d;
+			System.out.println(g);
+			
+			}
+
+		}
