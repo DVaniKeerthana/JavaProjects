bb64e53ac0e9cb80ea0dc37d4c93a48d76ca2086
Vani Keerthana <dandavanikeerthana@gmail.com>
2024-02-28 12:49:39 +0530

    loops
    forloop

---
+++ Feb27_forloop/src/test/SampleG.java
@@ -0,0 +1,20 @@
+package test;
+
+public class SampleG {
+
+	public static void main(String[] args) {
+		 int fact=5;
+		 int output=1;
+		 int a=1;
+		 do
+		 {
+			 output=output*a;
+			 System.out.println(output);
+			 a=a+1;
+			 
+		 }while(a<=fact);
+		 
+
+	}
+
+}
--- Feb27_forloop/src/test/SampleH.java
+++ Feb27_forloop/src/test/SampleH.java
@@ -1,20 +1,18 @@
 package test;
 
-public class SampleF {
+public class SampleH {
 
 	public static void main(String[] args) {
-		int fact=10;
-		int output =1;	
-		int a=1;
-		while(a<=fact)
+		int fact=7;
+		int output=1;
+		for(int a=1;a<=10;a=a+1)
 		{
-			
 			output=output*a;
 			System.out.println(output);
-			a=a+1;
+			
+		
 
 	}
 
 }
-	
 }
