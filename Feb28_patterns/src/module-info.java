a7d9cb451322eb733f10f5f49844ca761ca16bc0
Vani Keerthana <dandavanikeerthana@gmail.com>
2024-03-01 12:23:58 +0530

    Patterns
    Symbols,numbers

---
+++ Feb28_patterns/src/module-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module Feb28_patterns {
+}
---
+++ Feb28_patterns/src/test/SampleC.java
@@ -0,0 +1,16 @@
+package test;
+
+public class SampleC {
+
+	public static void main(String[] args) {
+		for(int a=5;a>=1;a=a-1)
+		{
+		for(int b=a;b>=1;b=b-1)
+		{
+			System.out.print(b);
+		}
+		    System.out.println();
+		}
+	}
+
+}
